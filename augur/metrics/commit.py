cfee0582ddde4dc4dd27307d5e5efa2b065db44a
Tjw38t <78340130+Tjw38t@users.noreply.github.com>
2021-12-15 18:26:34 -0600

    added email param to lines_changed_by_email

--- augur/metrics/commit.py
+++ augur/metrics/commit.py
@@ -369,7 +369,7 @@ def top_committers(self, repo_group_id, repo_id=None, year=None, threshold=0.5):
     return results
 
 @register_metric()
-def lines_changed_by_email(self, repo_group_id, repo_id=None):
+def lines_changed_by_email(self, repo_group_id, eParam="s@goggins.com", repo_id=None):
     """
     based on lines_changed_by_author
     Returns number of lines changed by specified author per day
@@ -381,22 +381,22 @@ def lines_changed_by_email(self, repo_group_id, repo_id=None):
             SELECT cmt_author_email, date_trunc('week', cmt_author_date::date) as cmt_author_date, cmt_author_affiliation as affiliation,
                 SUM(cmt_added) as additions, SUM(cmt_removed) as deletions, SUM(cmt_whitespace) as whitespace, repo_name
             FROM commits JOIN repo ON commits.repo_id = repo.repo_id
-            WHERE commits.repo_id = :repo_id
+            WHERE commits.repo_id = :repo_id and commits.cmt_author_email = eParam
             GROUP BY commits.repo_id, date_trunc('week', cmt_author_date::date), cmt_author_affiliation, cmt_author_email, repo_name
             ORDER BY date_trunc('week', cmt_author_date::date) ASC;
         """)
-        results = pd.read_sql(linesChangedByAuthorSQL, self.database, params={"repo_id": repo_id})
+        results = pd.read_sql(linesChangedByAuthorSQL, self.database, params={"repo_id": repo_id, "eParam": eParam})
         return results
     else:
         linesChangedByAuthorSQL = s.sql.text("""
             SELECT cmt_author_email, date_trunc('week', cmt_author_date::date) as cmt_author_date, cmt_author_affiliation as affiliation,
                 SUM(cmt_added) as additions, SUM(cmt_removed) as deletions, SUM(cmt_whitespace) as whitespace
             FROM commits
-            WHERE repo_id in (SELECT repo_id FROM repo WHERE repo_group_id=:repo_group_id)
+            WHERE repo_id in (SELECT repo_id FROM repo WHERE repo_group_id=:repo_group_id) and cmt_author_email = eParam
             GROUP BY repo_id, date_trunc('week', cmt_author_date::date), cmt_author_affiliation, cmt_author_email
             ORDER BY date_trunc('week', cmt_author_date::date) ASC;
         """)
-        results = pd.read_sql(linesChangedByAuthorSQL, self.database, params={"repo_group_id": repo_group_id})
+        results = pd.read_sql(linesChangedByAuthorSQL, self.database, params={"repo_group_id": repo_group_id, "eParam": eParam})
         return results
 
 @register_metric()
